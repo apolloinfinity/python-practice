e0ff3793f3f7175afc7513afdfebe7ca2821b6e2
Javier Ramirez <apollo.infinity88@gmail.com>
2017-04-29 00:23:26 -0700

    Chapter 5 Challanges
    Chapter 5 challanges from The Self-taught Programmer.

---
+++ Basics/chapter-5-challenges.py
@@ -0,0 +1,15 @@
+#create a list of your favorite musicians
+
+fav_musicians = ["Pantera",
+                 "Michael McCann",
+                 "Zakk Wylde",
+                 "Monuments",
+                 "Hans Zimmer"
+                 ]
+
+#create a list of tuples with each tuple containing the longitude and latitude of where I have been
+
+visited = [(-115.172813, 36.114647),
+           (-122.446747, 37.733795),
+           (-118.243683, 34.052235),
+           (-99.133209, 19.432608)]
