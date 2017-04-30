e57094f06b891519b91adfea5a99dfede975b5c8
Javier Ramirez <apollo.infinity88@gmail.com>
2017-04-29 23:22:20 -0700

    Adding to chapter 5 challenges

--- Basics/chapter-5-challenges.py
+++ Basics/chapter-5-challenges.py
@@ -13,3 +13,15 @@ visited = [(-115.172813, 36.114647),
            (-122.446747, 37.733795),
            (-118.243683, 34.052235),
            (-99.133209, 19.432608)]
+
+#Create a dictionary that has different attributes about yourself.
+
+javier = {"age":
+          "29",
+          "height":
+          "6",
+          "favorite color":
+          "cerulean",
+          "favorite author":
+          "Cory Althoff"
+    }
