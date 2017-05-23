0596b4b456b6fb800effc108238ee2fc0e2415e6
Javier Ramirez <apollo.infinity88@gmail.com>
2017-05-22 23:03:51 -0700

    Adding lists with other lists

--- Basics/for-loops-pt-3.py
+++ Basics/for-loops-pt-3.py
@@ -2,4 +2,14 @@
 for i in range(1,3):
     print(i)
     for letter in ["a", "b", "c"]:
-        print letter
+        print(letter)
+#First loop is the outer loop and the second loop is the inner loop.
+
+#You can use a nested loop to add numbers from another list
+list1 = [1,2,3,4]
+list2 = [5,6,7,8]
+added = []
+for i in list1:
+    for j in list2:
+        added.append(i + j)
+print(added)
