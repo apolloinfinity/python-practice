d6982620e8262d25e781297b08f939a5c1de7d94
Javier Ramirez <apollo.infinity88@gmail.com>
2017-05-22 23:07:50 -0700

    More nesting
    Nesting a for-loop inside of a while-loop.

--- Basics/for-loops-pt-3.py
+++ Basics/for-loops-pt-3.py
@@ -13,3 +13,9 @@ for i in list1:
     for j in list2:
         added.append(i + j)
 print(added)
+
+
+#Nesting a for-loop inside of a while-loop'
+while input('y or n?') != 'n':
+    for i in range(1, 6):
+        print(i)
