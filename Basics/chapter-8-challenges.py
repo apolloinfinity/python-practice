3bd28c41adb3303260930ac2074ba3a3b43ffa2c
Javier Ramirez <apollo.infinity88@gmail.com>
2017-05-31 00:17:19 -0700

    Chapter 8 challenges
    Finished

--- Basics/chapter-8-challenges.py
+++ Basics/chapter-8-challenges.py
@@ -1,4 +1,4 @@
 import statistics
-
+#1. Call a different function from the statistics module.
 nums = [100, 22, 42, 85, 64, 111, 9, 5, 57]
 print(statistics.median_high(nums))
---
+++ Basics/cubed.py
@@ -0,0 +1,4 @@
+#Chapter 8 challenge to create a cubed module and importing it into another module. 
+def cube(num):
+    return num**3
+
