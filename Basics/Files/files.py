0705f576ae699582639f565f4149a428e4a67721
Javier Ramirez <apollo.infinity88@gmail.com>
2017-06-14 09:56:18 -0700

    Open, writing, reading and closing

--- Basics/Files/files.py
+++ Basics/Files/files.py
@@ -2,3 +2,7 @@ fw = open('sample.txt', 'w')
 fw.write('Writing some stuff on my text file\n')
 fw.write('I like cheese')
 fw.close()
+
+st = open("st.txt", 'w')
+st.write("Hi, from Python")
+st.close()
