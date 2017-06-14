e16c2efe627c10112c70f1e0225eb887655040ef
Javier Ramirez <apollo.infinity88@gmail.com>
2017-06-14 09:40:27 -0700

    Moved Open-Write file
    Into a folder.

---
+++ Basics/Files/files.py
@@ -0,0 +1,4 @@
+fw = open('sample.txt', 'w')
+fw.write('Writing some stuff on my text file\n')
+fw.write('I like cheese')
+fw.close()
--- Basics/files.py
+++
@@ -1,6 +0,0 @@
-# OS module
-import os
-os.path.join("C:/Users/warner/", "python.txt")
-st = open("C:/Users/warner/python.txt", "w")
-st.write("Hi from Python!")
-st.close()
