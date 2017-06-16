ceb4dfba61fa1202df8a454fbe10bbd0ff285172
Javier Ramirez <apollo.infinity88@gmail.com>
2017-06-16 00:11:42 -0700

    Added the while logic but I'm still not finished with it

--- Basics/hangman.py
+++ Basics/hangman.py
@@ -14,3 +14,16 @@ def hangman(word):
     win = false
     print("Welcome to Hangman")
 
+
+while wrong < len(stages) - 1:
+    print("\n")
+    msg = "Guess a letter"
+    char = input(msg)
+    if char in rletters:
+        cind = rlettes \
+             .index(char)
+        board[cind] = char
+        rletters[cind] = '$'
+    else:
+        wrong += 1
+        print
