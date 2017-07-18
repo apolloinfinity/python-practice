9d6fe5daf3d529e83f90a4992c30f7e0af6a441c
Javier Ramirez <apollo.infinity88@gmail.com>
2017-07-17 21:51:57 -0700

    Added a bit more logic to Hangman Game

--- Basics/hangman.py
+++ Basics/hangman.py
@@ -1,29 +1,36 @@
 def hangman(word):
     wrong = 0
-    stages = stages = ["",
-             "________        ",
-             "|               ",
-             "|        |      ",
-             "|        0      ",
-             "|       /|\     ",
-             "|       / \     ",
-             "|               "
+    stages = ["",
+              "________ ",
+              "|        ",
+              "|   |    ",
+              "|   0    ",
+              "|  /|\   ",
+              "|  / \   ",
+              "|        "
               ]
     rletters = list(word)
-    board = ["__" * len(word)
-    win = false
-    print("Welcome to Hangman")
+    board = ["__"] * len( word)
+    win = False
+    print(" Welcome to Hangman")
 
-
-while wrong < len(stages) - 1:
-    print("\n")
-    msg = "Guess a letter"
-    char = input(msg)
-    if char in rletters:
-        cind = rlettes \
-             .index(char)
-        board[cind] = char
-        rletters[cind] = '$'
-    else:
-        wrong += 1
-        print
+    while wrong < len(stages) - 1:
+        print("\n")
+        msg = "Guess a letter"
+        char = input(msg)
+        if char in rletters:
+            cind = rlettes \
+                 .index(char)
+            board[cind] = char
+            rletters[cind] = '$'
+        else:
+            wrong += 1
+            print((" ".join(board)))
+            e = wrong + 1
+            print("\n"
+                  .join(stages[0: e]))
+            if "__" not in board:
+                 print("You win!")
+                 print(" ".join(board))
+                 win = True
+                 break
