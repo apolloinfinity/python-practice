05664e2ae85d87ac312eeec61fea8c1a701f9d07
Javier <apollo.infinity88@gmail.com>
2017-11-03 23:53:23 -0700

    created a simple area calculation program

---
+++ Basics/area_calc.py
@@ -0,0 +1,9 @@
+# name = input('What is your name punk?')
+# age = input('..and your age?')
+# print(name, 'you are', age)
+
+# Calculate the area of a circle
+radius = input('Input radius of your circle(ft)')
+area = 3.142*int(radius)**2
+print("THe area of your circle:", area)
+
