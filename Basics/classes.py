63fadc93c89b2bbca592434ec42e237a69e7e5bc
Javier <apollo.infinity88@gmail.com>
2017-11-26 22:25:12 -0800

    Finished example on how to use Classes with methods

--- Basics/classes.py
+++ Basics/classes.py
@@ -15,9 +15,20 @@ class User:
         self.first_name = name_pieces[0]
         self.last_name = name_pieces[-1]
 
-        def age(self):
-            """Return age of the user in years"""
-            today = datetime.date(2001, 5, 12)
+    def age(self):
+        """Return age of the user in years"""
+        today = datetime.date(2001, 5, 12)
+        yyyy = int(self.birthday[0:4])
+        mm = int(self.birthday[4:6])
+        dd = int(self.birthday[6:8])
+        dob = datetime.date(yyyy, mm, dd)  # Date of Birth
+        age_in_days = (today - dob).days
+        age_in_years = age_in_days / 365
+        return int(age_in_years)
+
+
+user = User("Javier Ramirez", "19710315")
+print(user.age())
 
 
 
