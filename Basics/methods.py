bac6d03ac5e6d6e3766be21391dcdc04821b37e5
Javier Ramirez <apollo.infinity88@gmail.com>
2018-01-30 21:10:34 -0800

    A file that explains how to find help and list the methods of a string

---
+++ Basics/methods.py
@@ -0,0 +1,14 @@
+c = "Hello"
+print(c.replace("l", "r"))
+
+# var.replace(old, new)
+
+# print(dir(c)) 
+# This function gives you a list of methods you can use with strings.
+
+print(c.upper())
+
+print(help("".replace))
+# help("".method) will give you a description of what the method does.
+
+# If a method has a square brackets, it means the parameter is optional
