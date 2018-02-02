84d1405fd244f37751814f2e48be3a500325df11
Javier Ramirez <apollo.infinity88@gmail.com>
2018-02-02 15:16:05 -0800

    Made the class so that multiple instances of Planets can be created

--- Advanced/classes.py
+++ Advanced/classes.py
@@ -1,7 +1,7 @@
 """
 A Class is a blueprint for how an object should behave
 classes start with a capital letter like objects in JS
-classes first function(method) acts is a constructor like in JS.4
+classes first function(method) acts is a constructor like in JS.
 You will see in the print function and orbit method, the letter f.
 It is new to Python 3.6 and it a formatted string literal. It can 
 also be used for formatting as I have done in this examples.
@@ -27,3 +27,9 @@ print(f'Radius is: {hoth.radius}')
 # print('It\'s gravity is {} times more than Earth\'s'.format(hoth.gravity))
 print(f'The gravity of {hoth.name} is {hoth.gravity} times more than Earth\'s.')
 print(hoth.orbit())
+
+naboo = Planet('Naboo', 300000, 8, 'Naboo System');
+print(f'Name: {naboo.name}')
+print(f'Radius: {naboo.radius}')
+print(f'Gravity: {naboo.gravity}')
+print(naboo.orbit())
