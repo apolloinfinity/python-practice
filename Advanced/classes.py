a74d94311c790bb3a0120ddcf4201c7d6eec4e52
Javier Ramirez <apollo.infinity88@gmail.com>
2018-02-02 13:58:49 -0800

    Refactored code to use formatted strings

--- Advanced/classes.py
+++ Advanced/classes.py
@@ -1,18 +1,28 @@
 """
 A Class is a blueprint for how an object should behave
 classes start with a capital letter like objects in JS
-classes first function(method) acts is a constructor like in JS.
+classes first function(method) acts is a constructor like in JS.4
+You will see in the print function and orbit method, the letter f.
+It is new to Python 3.6 and it a formatted string literal. It can 
+also be used for formatting as I have done in this examples.
 """
 
 class Planet():
     def __init__(self):
+        # This are properties
         self.name = 'Hoth'
         self.radius = 20000
         self.gravity = 5.5
         self.system = 'Hoth System'
+    
+    # This is a method
+    def orbit(self):
+        return f'{self.name} is orbiting in the {self.system}'
 
 
 hoth = Planet()
 
-print(hoth.name)
-print('It\'s gravity is {} times more than Earth\'s'.format(hoth.gravity))
+print(f'Name is: {hoth.name}')
+print(f'Radius is: {hoth.radius}')
+# print('It\'s gravity is {} times more than Earth\'s'.format(hoth.gravity))
+print(f'The gravity of {hoth.name} is {hoth.gravity} times more than Earth\'s.')
