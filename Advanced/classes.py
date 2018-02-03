5ef7bcd362a77b971e304cced103da19e45c8a65
Javier Ramirez <apollo.infinity88@gmail.com>
2018-02-02 16:01:00 -0800

    Used doctstrings to explain each part of a class

--- Advanced/classes.py
+++ Advanced/classes.py
@@ -1,25 +1,33 @@
 """
-A Class is a blueprint for how an object should behave
-classes start with a capital letter like objects in JS
-classes first function(method) acts is a constructor like in JS.
-You will see in the print function and orbit method, the letter f.
-It is new to Python 3.6 and it a formatted string literal. It can 
-also be used for formatting as I have done in this examples.
+A Class is a blueprint for how an object should behave.
+1. Class names must start a capital letter.
+2. Is a reserved method for python classes and must be declared. It is a constructor that initializes the class
+3. Methods are functions inside of classes. 
+4. Created an instance of the Planet object with properties.
+5. f' is a string formatter new to Python 3.6.
+6. Class level attributes are put in the head of the class before the __init__ method. They are owned by the class itself. They have the same value for every class. In this example, all the planets are round.
 """
-
+# 1. Declaring a class
 class Planet():
+
+    # 6. Class Level attributes
+    shape = 'round'
+
+    #. Creating the Initial method
     def __init__(self, name, radius, gravity, system):
-        # This are properties
+        # 2. Class properties
         self.name = name
         self.radius = radius
         self.gravity = gravity
         self.system = system
     
-    # This is a method
+    # 3. Method
     def orbit(self):
+        # 5. The f' is a formatter string literal.
         return f'{self.name} is orbiting in the {self.system}'
 
 
+# 4. Creating an instance of the object
 hoth = Planet('Hoth', 20000, 5.5, 'Hoth System')
 
 print(f'Name is: {hoth.name}')
@@ -28,7 +36,7 @@ print(f'Radius is: {hoth.radius}')
 print(f'The gravity of {hoth.name} is {hoth.gravity} times more than Earth\'s.')
 print(hoth.orbit())
 
-naboo = Planet('Naboo', 300000, 8, 'Naboo System');
+naboo = Planet('Naboo', 300000, 8, 'Naboo System')
 print(f'Name: {naboo.name}')
 print(f'Radius: {naboo.radius}')
 print(f'Gravity: {naboo.gravity}')
