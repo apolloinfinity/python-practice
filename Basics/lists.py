554d41a7db5b988522bc5614e60b4f8981f263f3
Javier Ramirez <apollo.infinity88@gmail.com>
2018-02-19 16:24:37 -0800

    List comprenhensions and learning pythonic way for lists

--- Basics/lists.py
+++ Basics/lists.py
@@ -13,3 +13,29 @@ list_numbers = list(set_numbers)
 print(list_numbers)
 
 print(type(list_numbers))
+
+# for value in range(1, 230):
+#     print(value)
+
+# Skipping Numbers
+even_numbers = list(range(2, 11, 2))
+print(even_numbers)
+
+# Creating squares with for loop and range method
+squares = []
+for value in range(1, 11):
+    square = value**2
+    squares.append(square)
+
+print(squares)
+
+# More consise way of writing the same problem above
+squared = []
+for val in range(1,11):
+    squared.append(val**2)
+
+print(squared)
+
+# More pythonic way of doing the same problem as a list comprenhension
+squareds = [value**2 for value in range(1,14)]
+print(squareds)
