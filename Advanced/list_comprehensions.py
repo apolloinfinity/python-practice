b850cd220c1351c0ca95e5ed385956df1e68aede
Javier <apollo.infinity88@gmail.com>
2018-02-25 19:20:10 -0800

    Practicing list comprehensions with math operations

--- Advanced/list_comprehensions.py
+++ Advanced/list_comprehensions.py
@@ -19,4 +19,7 @@ print(multiply_2)
 multiply_2_even = [value*2 for value in range(20) if value%2==0]
 print(multiply_2_even)
 
-# 
+# Possibly doing FizzBuzz with list comprehension??
+# This is my attempt. I will push this, even if it doesn't work
+
+# FizzBuzz = [fizz, buzz for value in range(1,20)if value%3 == 0 and value%5==0]
