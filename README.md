07ad9c98cd936ff3d9b314490e3e67692314e966
Javier Ramirez <apollo.infinity88@gmail.com>
2018-06-03 11:39:58 -0700

    Updating this repo

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# python
+# Python
 Repository of my Python code as I am learning
+
+## Python is a beautiful simple language. I picked up Python on the recommendation of a great friend.
+
+At the moment I have 3 folders
--- playground.py
+++ playground.py
@@ -1,3 +1,14 @@
-a = "Tricky"
-for i in a[:3]:
-    print(i)
+class TestClass():
+    def test1(self):
+        x = input('Enter a number: ')
+        return int(x)
+    
+    def test2(self, squared):
+        return squared ** 2
+
+
+y = TestClass()
+
+# print(y.test1(10))
+z = y.test1()
+print(y.test2(z))
